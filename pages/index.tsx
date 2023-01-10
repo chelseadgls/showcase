75549bfab135c11ec12aed210ce6e97a28519f72
chelseadgls <chelseamdouglas@gmail.com>
2023-01-10 13:03:46 -0500

    updated api calls

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,14 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 import Spaces from '../components/Spaces/Spaces'
 import Space_Card from '../components/Spaces/Space_Card'
+import Test from '../components/Test'
 
 export default function Home() {
   return (
-    <div className="container">
-        <Spaces />
-        <Space_Card />
+    <div>
+        {/* <Spaces />
+        <Space_Card /> */}
+        <Test />
     </div>
   )
 }
--- services/APIcalls.jsx
+++ services/APIcalls.jsx
@@ -1,37 +1,38 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export const retrieveSpaces = async () => {
-try {
-  const response = await axios.get(
-    "https://eozjtvk41svx4nc.m.pipedream.net"
-  );
-  // console.log(response.data);
-  return response.data;
-} catch (error) {
-  throw error;
-}
-};
 
-export const retrieveCreators = async () => {
-  try {
-    const response = await axios.get(
-      "https://eojbtbh68hot62m.m.pipedream.net"
-    );
-    // console.log(response.data);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-  };
+// export const retrieveCreators = async () => {
+//   try {
+//     const response = await axios.get(
+//       "https://showcase-backend-production.up.railway.app/creators"
+//     );
+//     // console.log(response.data);
+//     return response.data;
+//   } catch (error) {
+//     throw error;
+//   }
+//   };
 
-  export const retrieveItems = async () => {
-    try {
-      const response = await axios.get(
-        "https://eoc685b1ynuwbq.m.pipedream.net"
-      );
-      // console.log(response.data);
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-    };
+//   export const retrieveItems = async () => {
+//     try {
+//       const response = await axios.get(
+//         "https://showcase-backend-production.up.railway.app/items"
+//       );
+//       // console.log(response.data);
+//       return response.data;
+//     } catch (error) {
+//       throw error;
+//     }
+//     };
+
+// export const retrieveSpaces = async () => {
+// try {
+//   const response = await axios.get(
+//     "https://showcase-backend-production.up.railway.app/spaces"
+//   );
+//   // console.log(response.data);
+//   return response.data;
+// } catch (error) {
+//   throw error;
+// }
+// };
